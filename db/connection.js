28988bc0a767eedcd8bdf674b747baf9403df07a
DevOlaMicheal <olapademicheal3@gmail.com>
2024-02-02 14:05:17 +0100

    bug fix

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose')
 const connect = (uri) => {
     return mongoose.connect(uri, {
         useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,    
         useUnifiedTopology: true
 
     })
---
+++ sandbox.js
@@ -0,0 +1,5 @@
+const ob = [
+    
+]
+
+console.log(ob.length);
